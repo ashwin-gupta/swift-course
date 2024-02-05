bbc827a86183e0ecf650f07766652584cc11bf14
Ashwin Gupta <gupta.ashwin08@gmail.com>
2024-02-05 19:30:31 +1100

    Section - 14 - Completed

---
+++ Section-14/ByteCoin/Model/CoinData.swift
@@ -0,0 +1,15 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Ashwin Gupta on 5/2/2024.
+//  Copyright © 2024 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Codable {
+    
+    let asset_id_quote: String
+    let rate: Double
+}
